365a98e329e8b99a306a69749866f289c5893fac
abass77 <ibnouabass1691@gmail.com>
2020-11-26 01:05:54 +0100

    first commit

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -26,12 +26,51 @@ public class CalculatorTest {
  @Test
  public void minus() {
 
-  Assert.assertEquals(0, calculator.minus(2,2));
+  Assert.assertEquals(1, calculator.minus(3,2));
  }
 
  @Test
  public void multiply() {
-  Assert.assertEquals(4, calculator.multiply(2,2));
+  Assert.assertEquals(6, calculator.multiply(2,3));
+ }
+ @Test
+ public void testdivide(){
+  int   result=calculator.divide(6,2);
+  if(result !=3){
+   Assert.fail();
+  }
+ }
+
+ @Test
+ public void min() {
+  int min=calculator.min(15,25);
+  if (min !=15) Assert.fail();
+ }
+
+ @Test
+ public void max() {
+  int max=calculator.max(500,100);
+  Assert.assertEquals(500,max);
+
+ }
+
+ @Test
+ public void minElement() {
+  int tab[]={
+          1,5,6,9
+  };
+  int a=calculator.minElement(tab);
+  Assert.assertEquals(1,a);
+
+ }
+ @Test
+ public void maxElement(){
+  int tab[]={
+          1,4,5,9
+  };
+  int a =calculator.maxElement(tab);
+  Assert.assertEquals(9,a);
+
  }
 
  }
